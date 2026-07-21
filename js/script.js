334dca7d37e1259990eeabacd0f4fea2b6d0d6d0
gabenavarrette <57375982+gabenavarrette@users.noreply.github.com>
2026-07-20 20:02:42 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* ==================================================
+   Chapman Naz Website Scripts
+   ================================================== */
+
+
+/*
+    JavaScript will be added here as the website grows.
+
+    Future possibilities:
+    - Mobile navigation menu
+    - Form handling
+    - Planning Center integrations
+    - Sermon updates
+*/
+
+
+console.log("Chapman Naz website loaded.");
