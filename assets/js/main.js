500906189d6717ff661f3d086e67033b7d163214
gabenavarrette <57375982+gabenavarrette@users.noreply.github.com>
2026-07-20 19:41:34 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
+const toggle = document.getElementById("menuToggle");
+const menu = document.getElementById("navMenu");
 
+toggle.addEventListener("click", () => {
+
+    menu.classList.toggle("open");
+
+    toggle.textContent =
+        menu.classList.contains("open")
+            ? "✕"
+            : "☰";
+
+});
+
+<script src="assets/js/main.js"></script>
